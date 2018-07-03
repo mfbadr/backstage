ce8d0ccb6bf6b84c00c0f294c5799589dd2ef7e9
Mikey Badr <mikey.badr@attck.com>
2018-07-03 11:48:42 -0400

    calculator form updates

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 
 export default class CalculatorForm extends Component {
+  //TODO: Restrict to positive integers?
   constructor(props) {
     super(props);
+    this.state = {
+      inputValue: '0'
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
+
+  handleSubmit (event){
+    event.preventDefault();
+    // alert(this.state.inputValue);
+  }
+
+  handleChange (event){
+    this.setState({
+      inputValue:event.target.value
+    });
+  }
+
   render (){
     return (
       <div>
-        <form>
+        <form action="" onSubmit={this.handleSubmit}>
           <label>
             Input a number:
-            <input type="number" id="numberInput" name="numberInput"/>
+            <input
+              value={this.state.inputValue}
+              type="number"
+              id="numberInput"
+              onChange={this.handleChange}
+              name="numberInput"/>
           </label>
+          <button type="submit">Submit</button>
         </form>
       </div>
-      )
+    )
   }
 }
