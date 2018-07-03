a9dbad9feecdd33fa857b6d27d4b9529f81e2cfb
Mikey Badr <mikey.badr@attck.com>
2018-07-03 11:29:45 -0400

    add new form component to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CalculatorForm from './components/CalculatorForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Backstage Calculator</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>foo</code> and save to reload.
         </p>
+        <CalculatorForm/>
       </div>
     );
   }
 }
 
 export default App;
+
