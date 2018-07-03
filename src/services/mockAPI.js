cff442e15859d8b853951dee3afa656de423cdef
Mikey Badr <mikey.badr@attck.com>
2018-07-03 19:55:41 -0400

    implement closed loop solution

--- src/services/mockAPI.js
+++ src/services/mockAPI.js
@@ -1,15 +1,23 @@
 export default function calculateAsyc(inputNumber, occurences, lastDatetime){
   return new Promise(function(resolve, reject){
     if (true) {
-      let squareOfSums = 0;
-      for (let i = 0; i <= inputNumber; i++) {
-        squareOfSums += i;
-      }
+
+      //closed form solution
+      let squareOfSums = ((inputNumber/2)+.5)*inputNumber;
       squareOfSums = Math.pow(squareOfSums, 2)
-      let sumOfSquares = 0;
+      const sumOfSquares = inputNumber*(((inputNumber/3) + .5)*inputNumber + (1/6))
+
+      /***************
+      Alternate solutions using for loops
+      // let squareOfSums = 0;
+      // for (let i = 0; i <= inputNumber; i++) {
+        // squareOfSums += i;
+      // }
+      let sumOfSquares
       for (let n = 0; n <= inputNumber; n++) {
         sumOfSquares += Math.pow(n, 2);
-      }
+      // }
+      ************/
 
       const value = squareOfSums - sumOfSquares;
       const result =  {
